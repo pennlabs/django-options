5f9e51f913e350da6cc3596aa0269ca89c3c98b0
Armaan Tobaccowalla <armaan@tobaccowalla.com>
2020-02-21 18:14:53 -0500

    Updates

--- options/views.py
+++ options/views.py
@@ -1,15 +1,13 @@
 from django.http import JsonResponse
+from django.views import View
 
 from options.models import Option
 
 
-def option_json(request):
-    if request.method == "GET":
+class OptionListView(View):
+    def get(self, request):
         options = Option.objects.all()
         option_list = []
         for o in options:
             option_list.append({"key": o.key, "value": o.value})
         return JsonResponse({"Status": 200, "Options": option_list})
-
-    else:
-        return JsonResponse({"Status": 405, "Error": "Only GET requests allowed."}, status=405)
